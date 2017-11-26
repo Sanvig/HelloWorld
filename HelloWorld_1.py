5f049f861481b168bd43ff08350c67e5f6692059
Sanvig <heheheje@gma.com>
2017-11-26 12:43:29 +0100

    OK_2

--- HelloWorld_1.py
+++ HelloWorld_1.py
@@ -1,10 +1,27 @@
 
 x=47
 
+x1=50
+
 print("hello world")
 print(x)
+x=3
 
 print("Goodbye")
+
+x=10
+
+print(x/2)
+
+print("prøv igen")
+print(x)
+
+while x>1:
+	print("prøv igen") 
+	print(x)
+	x=x-1
+
+print(x1)
 
 
 
